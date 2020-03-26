7d1a3ea71c6309f9f62ad77f9d23b8f363791a2e
Shahzaib Anwar <shahzaibanwar009u097@gmail.com>
2020-03-26 13:36:43 +0500

    Add custum toasts

--- app/src/main/java/com/example/bookshop/MainActivity.java
+++ app/src/main/java/com/example/bookshop/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.bookshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showSuccessToast(View view)
+    {
+        Toast objectToast = new Toast(this); // making object of Toast // this is used for showing toast in current activity
+
+        objectToast.setGravity(Gravity.CENTER_HORIZONTAL,0,0);
+        objectToast.setDuration(Toast.LENGTH_LONG);
+
+        // layout inflator
+        LayoutInflater objectInfloator = getLayoutInflater();
+
+        View convertedView = objectInfloator.inflate(R.layout.success_toast_layout, (ViewGroup) findViewById(R.id.success_toast_layout), false);
+
+        objectToast.setView(convertedView);
+        objectToast.show();
+    }
+    public void showErrorToast(View view)
+    {
+        Toast objectToast = new Toast(this); // making object of Toast // this is used for showing toast in current activity
+
+        objectToast.setGravity(Gravity.CENTER_HORIZONTAL,0,0);
+        objectToast.setDuration(Toast.LENGTH_LONG);
+
+        // layout inflator
+        LayoutInflater objectInfloator = getLayoutInflater();
+
+        View convertedView = objectInfloator.inflate(R.layout.error_toast_layout, (ViewGroup) findViewById(R.id.error_toast_layout), false);
+
+        objectToast.setView(convertedView);
+        objectToast.show();
     }
 }
